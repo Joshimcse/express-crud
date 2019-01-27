d67b77c4205cf86555bdc980c9f1c3e860375358
joshimcse <joshimcse@gmail.com>
2019-01-27 18:19:16 +0600

    add comment

--- routes/users.js
+++ routes/users.js
@@ -9,16 +9,42 @@
 const router = require('express').Router();
 
 //Load Controller
-const { registerController, loginController, getUserController, getUsersController } = require('../controller/users');
+const {
+  registerController,
+  loginController,
+  getUserController,
+  getUsersController
+} = require('../controller/users');
 
 /**
- * @route POST api/users/register
- * @desc Register an user
+ * @route  POST api/users/register
+ * @desc   Register an user
  * @access Public
  */
 router.post('/register', registerController);
+
+/**
+ * @route  POST api/users/login
+ * @desc   Send user info to server && check it valid or not.
+ * @return If req is valid then return a token
+ * @access Public
+ */
 router.post('/login', loginController);
+
+/**
+ * @route  GET api/users/:id
+ * @desc   That's route for retrive single user.
+ * @return If req auth is valid then retun a particular user
+ * @access Public
+ */
 router.get('/:id', getUserController);
+
+/**
+ * @route  GET api/users/:id
+ * @desc   That's route for retrive all users.
+ * @return If req auth is valid then retun all user.
+ * @access Public
+ */
 router.get('/', getUsersController)
 
 module.exports = router;
